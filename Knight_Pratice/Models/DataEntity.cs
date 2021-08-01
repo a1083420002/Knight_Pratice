3bbf7b1a1a0c346dbfe4b1f0c1abcc99d18021b0
Jhen Ye <jhenye@nine-yi.com>
2021-08-01 09:52:11 +0800

    Add DataEntity

---
+++ Knight_Pratice/Models/DataEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Knight_Pratice.Models
+{
+    public class DataEntity
+    {
+        public int Id { get; set; }
+        public string Input { get; set; }
+        public string Result { get; set; }
+    }
+}
